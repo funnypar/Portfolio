88c90ffed38d15945d3ba3ccd2b0c920408d3f1a
mopano <p.r.7.norouzi@gamil.com>
2024-05-04 14:10:48 +0330

    addedPortfolio

---
+++ src/features/portfolio/PortfolioItem.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/prop-types */
+import { DiGithubAlt } from "react-icons/di";
+
+const PortfolioItem = ({ name, image, project, github, techs = [] }) => {
+    return (
+        <div className=" w-2/5 outline outline-3 outline-offset-8 rounded-lg outline-item ">
+            <a href={project}>
+                <img
+                    src={image}
+                    alt={image}
+                    className="rounded-xl hover:scale-125 transition-all duration-700"
+                />
+            </a>
+            <div className="flex flex-col justify-between mt-3">
+                <h6 className="text-center font-semibold">{name}</h6>
+                <p>
+                    Source :
+                    <a
+                        href={github}
+                        className="text-center ml-3 hover:font-normal hover:text-me"
+                    >
+                        Link
+                        <span>
+                            <DiGithubAlt className="text-xl inline-block ml-2" />
+                        </span>
+                    </a>
+                </p>
+                {techs.length > 0 && (
+                    <div>
+                        <h6>Technologies : </h6>
+                        <div className="flex flex-wrap gap-2 font-normal mt-2">
+                            {techs.map((el) => (
+                                <p
+                                    key={Math.random}
+                                    className="bg-me py-1 px-2 text-center rounded-md text-sm"
+                                >
+                                    {el}
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default PortfolioItem;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,87 @@
+import PortfolioItem from "../features/portfolio/PortfolioItem";
+
 const Portfolio = () => {
     return (
-        <div className="bg-rightSide rounded-l-3xl flex justify-center items-center flex-col text-header gap-6">
-            Portfolio
+        <div className="bg-rightSide rounded-l-3xl  text-header gap-6 px-16 py-10 font-roberto overflow-scroll">
+            <h3 className="text-3xl font-bold">Portfolio</h3>
+            <div className=" font-roberto font-light flex flex-wrap gap-12 mt-12">
+                <PortfolioItem
+                    name="Pizzamopano"
+                    image="/public/media/projects/1.png"
+                    project="https://pizzamopano.netlify.app/"
+                    github="https://github.com/funnypar/Learn-React/tree/main/pizza-mopano"
+                    techs={[
+                        "React",
+                        "React Router",
+                        "Redux",
+                        "Tailwind",
+                        "HTML",
+                    ]}
+                />
+                <PortfolioItem
+                    name="Mopanoreactredux"
+                    image="/public/media/projects/3.png"
+                    project="https://mopanoreactredux.netlify.app/"
+                    github="https://github.com/funnypar/Learn-React/tree/main/reduxBank"
+                    techs={["React", "Redux", "Redux-thunk", "HTML", "Css"]}
+                />
+                <PortfolioItem
+                    name="Footexam"
+                    image="/public/media/projects/4.png"
+                    project="https://footexam.netlify.app/"
+                    github="https://github.com/funnypar/Learn-React/tree/main/quiz"
+                    techs={[
+                        "React",
+                        "ApiContext",
+                        "useReducer",
+                        "Supabase",
+                        "HTML",
+                        "Css",
+                    ]}
+                />
+                <PortfolioItem
+                    name="Mopanofaraway"
+                    image="/public/media/projects/2.png"
+                    project="https://mopanofaraway.netlify.app/"
+                    github="https://github.com/funnypar/Learn-React/tree/main/faraway"
+                    techs={["React", "HTML", "Css"]}
+                />
+                <PortfolioItem
+                    name="Mopanoexchange"
+                    image="/public/media/projects/5.png"
+                    project="https://mopanoexchange.netlify.app/"
+                    github="https://github.com/funnypar/Learn-React/tree/main/exchange"
+                    techs={["React", "HTML", "Css"]}
+                />
+                <PortfolioItem
+                    name="Mopanotip"
+                    image="/public/media/projects/6.png"
+                    project="https://mopanotip.netlify.app/"
+                    github="https://github.com/funnypar/Learn-React/tree/main/tip"
+                    techs={["React", "HTML", "Css"]}
+                />
+                <PortfolioItem
+                    name="Mopanoowe"
+                    image="/public/media/projects/7.png"
+                    project="https://mopanoowe.netlify.app/"
+                    github="https://github.com/funnypar/Learn-React/tree/main/owe"
+                    techs={["React", "HTML", "Css"]}
+                />
+                <PortfolioItem
+                    name="Whatisfoss"
+                    image="/public/media/projects/8.png"
+                    project="https://whatisfoss.netlify.app/"
+                    github="https://github.com/funnypar/FOSS/tree/master"
+                    techs={["HTML", "Sass"]}
+                />
+                <PortfolioItem
+                    name="Whatisfoss"
+                    image="/public/media/projects/9.png"
+                    project="https://fsocietyy.netlify.app/"
+                    github="https://github.com/funnypar/fSociety"
+                    techs={["HTML", "Css"]}
+                />
+            </div>
         </div>
     );
 };
