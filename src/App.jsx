4412148b042cfcc27245fd936f12ab8999626266
mopano <p.r.7.norouzi@gamil.com>
2024-05-05 23:04:53 +0330

    addedLoader

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,23 @@ import Me from "./pages/Me";
 import Resume from "./pages/Resume";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import { Suspense } from "react";
+import { PacmanLoader } from "react-spinners";
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<AppLayout />}>
-                    <Route index element={<Home />} />
-                    <Route path="me" element={<Me />} />
-                    <Route path="resume" element={<Resume />} />
-                    <Route path="portfolio" element={<Portfolio />} />
-                    <Route path="contact" element={<Contact />} />
-                </Route>
-            </Routes>
+            <Suspense fallback={<PacmanLoader color="#36d7b7" />}>
+                <Routes>
+                    <Route path="/" element={<AppLayout />}>
+                        <Route index element={<Home />} />
+                        <Route path="me" element={<Me />} />
+                        <Route path="resume" element={<Resume />} />
+                        <Route path="portfolio" element={<Portfolio />} />
+                        <Route path="contact" element={<Contact />} />
+                    </Route>
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 }
